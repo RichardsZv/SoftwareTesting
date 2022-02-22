64881b248c4b701261e84ec5094c4b75fced3690
RichardsZv <richards.zvagulis@gmail.com>
2022-02-22 19:19:50 +0200

    Login steps added

--- test_code/features/login.feature
+++ test_code/features/login.feature
@@ -1,8 +1,14 @@
 Feature: Oracle Login test
 
-Scenario: Invalid login with user
+Scenario Outline: Invalid login with user
   Given User has opened Oracle Profale page
-  When User inputs wrong email
-  And User inputs wrong password
+  When User inputs <email> as the email
+  And User inputs <password> as the password
   And User presses sign in button
   Then User sees invalid credentials message
+
+  Examples:
+    | email                 | password  |
+    | dsagfghjkas@gmail.com | 124123521 |
+    | gdsageasghj@gmail.com | 21423155  |
+    | gdsags@gmail.com      | 21423     |
--- test_code/features/step-definitions/loginSteps.js
+++ test_code/features/step-definitions/loginSteps.js
@@ -1,12 +1,12 @@
 import { Given, When, Then } from "@wdio/cucumber-framework";
 import {tests} from "../support/tests";
 
-When("User inputs wrong email", async function () {
-  await tests.loginTest.fillEmailField("sdafasgh@gmail.com");
+When("User inputs {word} as the email", async function (email) {
+  await tests.loginTest.fillEmailField(email);
 });
 
-When("User inputs wrong password", async function () {
-  await tests.loginTest.fillPosswordField("dasgdsag");
+When("User inputs {word} as the password", async function (password) {
+  await tests.loginTest.fillPosswordField(password);
 });
 
 Then("User sees invalid credentials message", async function () {
