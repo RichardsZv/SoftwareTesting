3c7736ea7d525bf9e98fecf28e917e8a75401d17
RichardsZv <richards.zvagulis@gmail.com>
2022-02-24 20:00:53 +0200

    removed node modules and other from github

--- js_intro/export.js
+++
@@ -1,5 +0,0 @@
-export const name = "Ričards";
-
-export function sum(x, y){
-    return x+y;
-}
--- js_intro/import.js
+++
@@ -1,4 +0,0 @@
-import {name, sum} from "./export.js"
-console.log(name);
-let add = sum(4, 8); 
-console.log(add);
